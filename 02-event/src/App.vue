979766bcadf810a83b7b0f6b7f7b9643a213b8f9
dam anh nam <damnam2k3@gmail.com>
2025-12-16 10:51:32 +0700

    vue

---
+++ 02-event/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+
+const greet = (event) => {
+  alert(`Xin chào mọi người - event: ${event.type}`);
+};
+
+const sayHi = (name) => {
+  alert(`Xin chào ${name}`);
+};
+</script>
+
+<template>
+  <div>
+    <h1>Event handling</h1>
+
+    <button @click="count++">click de tang</button>
+    <p>Count: {{ count }}</p>
+
+    <button @click="greet">chào</button>
+
+    <button @click="sayHi('Nam')">chào Nam</button>
+    <button @click="sayHi('Anh')">chào Anh</button>
+  </div>
+</template>
