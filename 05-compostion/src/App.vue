90fb549f26a20a63ac4160178e31b9171ba8d731
dam anh nam <damnam2k3@gmail.com>
2025-12-17 13:53:25 +0700

    vue

---
+++ 05-compostion/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { computed, ref } from 'vue';
+  const count = ref(0)
+  const increase =()=> count.value++
+  const decrease = () => {
+  if (count.value > 0) count.value--
+}
+
+ 
+
+
+const status = computed(() => {
+  return count.value % 2 === 0 ? "số chẵn" : "số lẻ"
+})
+</script>
+
+<template>
+ <div>
+  <h2>Compostion demo</h2>
+  <p>Count: {{ count }}</p>
+  <p>{{ status }}</p>
+  <button @click="increase">Tăng lên</button>
+  <button @click="decrease">Giảm xuống</button>
+
+ </div>
+</template>
+
+<style scoped>
+
+
+</style>
